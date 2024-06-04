19caff68d3e22ee1b459bcc1a6cac54da33cc161
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-06-04 14:37:21 -0300

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,30 +1,41 @@
 #include <Wire.h>
 
-const int boton=36;
+// Definimos los pines
+const int button = 36;  // Pin donde está conectado el botón
 
-int buttonState=0;
-int i=0;
+
+// Variable para guardar el estado del botón
+int buttonState = 0;
+
+// Variable para asegurarnos de que la acción se ejecuta solo una vez
+bool actionExecuted = false;
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(boton,INPUT);
+  // Configuramos el pin del botón como entrada
+  pinMode(button, INPUT_PULLUP);
   Wire.begin();
-  Wire.beginTransmission(0xBA);
-  Wire.write(0x07);
-  Wire.write(0x00);
-  Wire.write(0x02);
-  Wire.endTransmission();
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  buttonState = digitalRead(boton);
-  if (buttonState==HIGH) {
-      for (i=0;i<1;i++){
+  // Leemos el estado del botón
+  buttonState = digitalRead(button);
+
+  // Si el botón está presionado (LOW debido al pull-up)
+  if (buttonState == LOW && !actionExecuted) {
+    // Ejecutamos la serie de acciones
+    executeActions();
+
+    // Marcamos la acción como ejecutada para no repetirla
+    actionExecuted = true;
+  }
+
+}
+
+void executeActions() {
           Wire.beginTransmission(0xBA);
           Wire.write(0x07);
           Wire.write(0x00);
           Wire.write(0x02);
           Wire.endTransmission();
-        }  
-    }
 }
