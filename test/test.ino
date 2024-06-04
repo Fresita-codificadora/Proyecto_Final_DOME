af42249470273cc45b32d93d10b3a78bfe492f21
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-06-04 14:30:51 -0300

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,16 +1,30 @@
 #include <Wire.h>
 
+const int boton=36;
+
+int buttonState=0;
+int i=0;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(boton,INPUT);
   Wire.begin();
   Wire.beginTransmission(0xBA);
   Wire.write(0x07);
   Wire.write(0x00);
   Wire.write(0x02);
-  Wire.endTransmission(stop);
+  Wire.endTransmission();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  buttonState = digitalRead(boton);
+  if (buttonState==HIGH) {
+      for (i=0;i<1;i++){
+          Wire.beginTransmission(0xBA);
+          Wire.write(0x07);
+          Wire.write(0x00);
+          Wire.write(0x02);
+          Wire.endTransmission();
+        }  
+    }
 }
