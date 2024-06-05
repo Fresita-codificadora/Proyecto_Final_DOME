6d4888383d488fa7bdb1ef0d29e05cdf20b12e6e
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-06-05 16:56:28 -0300

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,20 +1,33 @@
 #include <Wire.h>
+#include "esp32-hal-timer.h"
 
 // Definimos los pines
 const int button = 36;  // Pin donde está conectado el botón
-
+const int boton_3v3=39; 
+const int reset =18;
+const int trigg=19;
 
 // Variable para guardar el estado del botón
-int buttonState = 0;
+int buttonState = 0; 
 
 // Variable para asegurarnos de que la acción se ejecuta solo una vez
 bool actionExecuted = false;
 
+
+
 void setup() {
   // Configuramos el pin del botón como entrada
   pinMode(button, INPUT_PULLUP);
+  pinMode(reset,OUTPUT);
+  pinMode(trigg,OUTPUT);
+  pinMode(boton_3v3,OUTPUT);
+  digitalWrite(reset,HIGH);
+  digitalWrite(trigg,LOW);
+  digitalWrite(boton_3v3,HIGH);
   Wire.begin();
- 
+
+
+
 }
 
 void loop() {
@@ -33,7 +46,7 @@ void loop() {
 }
 
 void executeActions() {
-          Wire.beginTransmission(0xBA);
+          Wire.beginTransmission(B1011101);
           Wire.write(0x07);
           Wire.write(0x00);
           Wire.write(0x02);
