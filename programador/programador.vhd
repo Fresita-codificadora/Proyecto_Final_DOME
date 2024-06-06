e50496e31067f47e04850ed95e221e4fe52d0a6e
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-06-06 17:40:41 -0300

    cambios menores en programador.vhd

--- programador/programador.vhd
+++ programador/programador.vhd
@@ -1,7 +1,7 @@
 -- ARCHIVO: programador.vhd
 -- DESCRIPCION: maquina de estados, encagada de implementar el protocolo similar al i2c para la camara 
 -- ESTADOS:
---    idle : Estado inicial donde se espera a la seÃ±al start
+--    idle : Estado inicial donde se espera a la señal start
 --    start : Estado donde se inicia el proceso de envio de datos, mandando el bit start
 --    b_trans : estado de transicion para la escritura donde se cambia el bit a enviar
 --    b_write : estado de escitura del bit donde se pone el clk en alto para que se realice la escritura
@@ -9,20 +9,20 @@
 --    ack_2 : estado donde se pulsa el clk y se espera eller el bit de ack
 --    error : estado de error cuando no se recibe un ack, hay que reiniciar la maquina de estados
 --    idle_2 : estado de idle donde no hayq que pasar por el estado start, y ya estamos listos para mandar otro byte
---    stop_1 : estado donde se manda la seÃ±al de stop
---    stop_2 : estado donde se manda la segunda parte de la seÃ±al de stop
+--    stop_1 : estado donde se manda la señal de stop
+--    stop_2 : estado donde se manda la segunda parte de la señal de stop
 -- PUERTOS:
 --    clk: clock de entrada a el doble de la frecuencia que la esperada en el puerto i2c, en mi caso debo tener en cuenta que la restriccion de la camara
 --         me pide que el flanco al menos sea de 5 clk del clk maestro por lo que este clk como maximo es 5 veces menos que el clk maestro
---    s_t: entrada, seÃ±al de start que comienza la maquina de estados
---    r_w: entrada, seÃ±al que le indica a la maquina de estados que puede escribir el proximo byte luego de estar en idle_2, debemos haber cambiado los datos de entrada antes de accionar esta seÃ±al
---    stop: entrada, seÃ±al que le indica que comience el proceso de stop
---   	reset : entrada, seÃ±al que reinicia la maquina de estados estas  
---    data_in : entrada, seÃ±al paralela del lato que tiene que enviar
---    sda_o : salida, seÃ±al serie de salida de datos con un buffer tristae
---    sca_o : salida, seÃ±al de salida de clk para la salida de datos serie
---    err : salida, seÃ±al de error cuando no se tiene la seÃ±al de ack
---    buisy : salida, seÃ±al que indica que se esta mandando un byte y que se debe mantener los datos
+--    s_t: entrada, señal de start que comienza la maquina de estados
+--    r_w: entrada, señal que le indica a la maquina de estados que puede escribir el proximo byte luego de estar en idle_2, debemos haber cambiado los datos de entrada antes de accionar esta señal
+--    stop: entrada, señal que le indica que comience el proceso de stop
+--   	reset : entrada, señal que reinicia la maquina de estados estas  
+--    data_in : entrada, señal paralela del lato que tiene que enviar
+--    sda_o : salida, señal serie de salida de datos con un buffer tristae
+--    sca_o : salida, señal de salida de clk para la salida de datos serie
+--    err : salida, señal de error cuando no se tiene la señal de ack
+--    buisy : salida, señal que indica que se esta mandando un byte y que se debe mantener los datos
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -72,7 +72,7 @@ begin
 			case state is
 				when idle=>
 					if s_t = '1' then
-						state <= start;       -- idle ppal donde esperamos la seÃ±al de start(s_t) para comenzar a mandar la primera direccion
+						state <= start;       -- idle ppal donde esperamos la señal de start(s_t) para comenzar a mandar la primera direccion
 						data <= data_in;
 					else
 						state <= idle;
@@ -81,7 +81,7 @@ begin
 				when start=>
 					state<=start_2;
 				when start_2=>
-					count <= 7;                 -- recibimos la seÃ±al de start y mandamos el start bit
+					count <= 7;                 -- recibimos la señal de start y mandamos el start bit
 					state <= b_trans;
 				when b_trans=>                 -- estado intermedio donde pulsamos el clk de datos
 					state <= b_write_1;
@@ -115,7 +115,7 @@ begin
 					if r_w = '1' then      --estado idle donde podemos mandar el proximo dato
 						state <= b_trans;
 						data <= data_in;
-					elsif stop = '1' then      -- si llega la seÃ±al de stop lo que hacemos es mandar el stop bit 
+					elsif stop = '1' then      -- si llega la señal de stop lo que hacemos es mandar el stop bit 
 						state <= stop_1;
 					else
 						state <= idle_2;       -- si no llegan comandos nos quedamos aca esperando 
@@ -138,7 +138,7 @@ begin
 	process (state,data,count,sda_o)
 	begin
 		case state is
-			when idle =>        -- en este estado ambas lineas la de datos y clk deben estar en alto, y la seÃ±al de buisy debe estar en bajo
+			when idle =>        -- en este estado ambas lineas la de datos y clk deben estar en alto, y la señal de buisy debe estar en bajo
 				sca <= '1';      --clk= 1
 				sda <= '1';       --dato= 1
 				buisy <= '0';
