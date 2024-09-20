757d2013dadad123a2385fc53f8f9eb2775c466c
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-09-20 14:09:54 -0300

    updates_algorimo

--- Pruebas/prueba_algoritmo/simulation/qsim/pruebas_algoritmo.do
+++ Pruebas/prueba_algoritmo/simulation/qsim/pruebas_algoritmo.do
@@ -1,11 +1,11 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work pruebas_algoritmo.vo
-vlog -work work Waveform.vwf.vt
-vsim -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.test_uart_tx_rx_vlg_vec_tst
+vlog -work work Waveform1.vwf.vt
+vsim -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.uart_algo_vlg_vec_tst
 vcd file -direction pruebas_algoritmo.msim.vcd
-vcd add -internal test_uart_tx_rx_vlg_vec_tst/*
-vcd add -internal test_uart_tx_rx_vlg_vec_tst/i1/*
+vcd add -internal uart_algo_vlg_vec_tst/*
+vcd add -internal uart_algo_vlg_vec_tst/i1/*
 proc simTimestamp {} {
     echo "Simulation time: $::now ps"
     if { [string equal running [runStatus]] } {
---
+++ Pruebas/prueba_algoritmo/uart_algo.vhd
@@ -0,0 +1,77 @@
+-- Quartus Prime VHDL Template
+-- Four-State Moore State Machine
+
+-- A Moore machine's outputs are dependent only on the current state.
+-- The output is written only when the state changes.  (State
+-- transitions are synchronous.)
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity uart_algo is
+	generic(
+			pixels : integer := 1_310_720
+	);
+	port(
+		clk			: in	std_logic;
+		reset	 		: in	std_logic;
+		rx_dv	 		: in std_logic;
+		rx_byte 		: in std_logic_vector (7 downto 0);
+		pix_count	: out integer range 0 to 1_310_720;
+		byte_o	: out std_logic_vector(7 downto 0)
+	);
+
+end entity;
+
+architecture rtl of uart_algo is
+
+	-- Build an enumerated type for the state machine
+	type state_type is (idle, envio);
+
+	-- Register to hold the current state
+	signal state   : state_type;
+	
+	signal pix_count_int : integer range 0 to pixels;
+	signal reg_data : std_logic_vector (7 downto 0);
+begin
+	
+	-- Logic to advance to the next state
+	process (all)
+	begin
+		if reset = '0' then
+			state <= idle;
+			pix_count_int 		<= 0;
+			reg_data	<= (others=>'0');
+		elsif (rising_edge(clk)) then
+			case state is
+				when idle=>
+					if rx_dv = '1' then
+						state <= envio;
+						pix_count_int <= pix_count_int + 1;
+						reg_data <= rx_byte;
+					else
+						state <= idle;
+						if pix_count_int = 1_310_720 then
+							pix_count_int <= 0;
+						end if;
+					end if;
+				when envio=>
+					state<=idle;
+			end case;
+		end if;
+	end process;
+
+	-- Output depends solely on the current state
+	process (all)
+	begin
+		case state is
+			when idle =>
+				pix_count	<=	pix_count_int;
+				byte_o		<= reg_data;
+			when envio=>
+				pix_count	<=	pix_count_int;
+				byte_o		<= reg_data;
+		end case;
+	end process;
+
+end rtl;
