0078e425873c8dcac1e71afaa44d5c2b9a89889a
Fresita-codificadora <72816373+Fresita-codificadora@users.noreply.github.com>
2024-09-21 20:48:00 -0300

    Update uart_test_3.m

--- Pruebas/prueba_algoritmo/uart_test_3.m
+++ Pruebas/prueba_algoritmo/uart_test_3.m
@@ -1,5 +1,6 @@
 % Script de prueba para enviar y recibir datos por puerto serie
-
+clear
+clc
 % Configuración del puerto serie
 port_name = 'COM3';  % Cambia este valor por el puerto serie correcto en tu sistema
 baud_rate = 9600;    % Asegúrate de que coincida con la configuración del dispositivo
@@ -11,10 +12,17 @@ serial_port = serialport(port_name, baud_rate, 'Timeout', 10);  % Timeout de 10 
 configureTerminator(serial_port, "LF");  % Línea de finalización (line feed)
 
 % Vector de datos a enviar (cada valor debe ser <= 255)
+<<<<<<< Updated upstream
 datos_a_enviar = [100, 150, 200];  % Puedes cambiar estos valores
 
 % Llamada a la función para enviar y recibir datos
 enviar_y_recibir_datos(serial_port, datos_a_enviar);
+=======
+datos_a_enviar = [1, 2, 0, 0, 0, 3, 4, 0, 0, 0, 5 ,6, 0, 0,0 ,7 ,8,0,0,0,9,10,0,0,0,11,12,0,0,0,13,14];  % Puedes cambiar estos valores
+%datos_a_enviar = uint8(255*rand(1,100));
+% Llamada a la función para enviar y recibir datos
+vector_recibido=enviar_y_recibir_datos(serial_port, datos_a_enviar);
+>>>>>>> Stashed changes
 
 % Cerrar el puerto serie
 clear serial_port;
